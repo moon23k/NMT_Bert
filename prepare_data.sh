ee89cbc3dd1ef5aac090620a4ebaf704de53e74f
moon <d.moon23k.gmail.com>
2022-03-24 22:19:27 +0900

    Data PipeLine modified

--- prepare_data.sh
+++ prepare_data.sh
@@ -2,36 +2,39 @@
 mkdir -p data
 cd data
 
-datasets=(wmt_sm iwslt_sm multi30k)
 splits=(train valid test)
 langs=(en de)
 models=(bert albert roberta)
 
 
-#Download datasets
-bash ../scripts/download_sm_datasets.sh
+#download small wmt dataset
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=11ln6BiC4l1kk-vCvKGgQngSzoDOai4iP' -O wmt_sm.tar.gz
+tar -zxvf wmt_sm.tar.gz
+rm wmt_sm.tar.gz
+
+
+#move original files to raw folder
+cd wmt_sm
+mkdir -p raw
+mv `ls | grep -v raw` raw
+cd ..
+
 
 #Peer Tokenize with sacremoses
 python3 -m pip install -U sacremoses
-for dataset in "${datasets[@]}"; do
-    for split in "${splits[@]}"; do
-        for lang in "${langs[@]}"; do
-            mkdir -p ${dataset}/tok
-            sacremoses -l ${lang} -j 8 tokenize < ${dataset}/raw/${split}.${lang} > ${dataset}/tok/${split}.${lang}
-        done
+for split in "${splits[@]}"; do
+    for lang in "${langs[@]}"; do
+        mkdir -p wmt_sm/tok
+        sacremoses -l ${lang} -j 8 tokenize < wmt_sm/raw/${split}.${lang} > wmt_sm/tok/${split}.${lang}
     done
 done
 
 
-
 #concat all files into one file to build vocab
-for dataset in "${datasets[@]}"; do
-    cat ${dataset}/tok/* >> ${dataset}/concat.txt
-done
+cat wmt_sm/tok/* >> wmt_sm/concat.txt
 
 
-
-#Build vocab
+#Get Sentencepiece
 git clone https://github.com/google/sentencepiece.git
 cd sentencepiece
 mkdir build
@@ -43,31 +46,25 @@ sudo ldconfig
 cd ../../
 
 
-
-for dataset in "${datasets[@]}"; do
-    mkdir -p ${dataset}/vocab
-    for model in "${models[@]}"; do
-        bash ../scripts/build_vocab.sh -i ${dataset}/concat.txt -p ${dataset}/vocab/${model} -m ${model}
-    done
-    rm ${dataset}/concat.txt
+#Build Vocab with Sentencepice
+mkdir -p wmt_sm/vocab
+for model in "${models[@]}"; do
+    bash ../scripts/build_vocab.sh -i wmt_sm/concat.txt -p wmt_sm/vocab/${model} -m ${model}
 done
+rm wmt_sm/concat.txt
 
 
 
-#tokens to ids
-for dataset in "${datasets[@]}"; do
-    for model in "${models[@]}"; do
-        mkdir -p ${dataset}/ids_${model}
-        for split in "${splits[@]}"; do
-            for lang in "${langs[@]}"; do
-                spm_encode --model=${dataset}/vocab/${model}.model --extra_options=bos:eos \
-                --output_format=id < ${dataset}/tok/${split}.${lang} > ${dataset}/ids_${model}/${split}.${lang}
-            done
+#convert tokens to ids
+for model in "${models[@]}"; do
+    mkdir -p wmt_sm/ids_${model}
+    for split in "${splits[@]}"; do
+        for lang in "${langs[@]}"; do
+            spm_encode --model=wmt_sm/vocab/${model}.model --extra_options=bos:eos \
+            --output_format=id < wmt_sm/tok/${split}.${lang} > wmt_sm/ids_${model}/${split}.${lang}
         done
     done
 done
 
 
-
 rm -r sentencepiece
-
